26393fbd09c359a88fbcc7d68fd99e60d2c7b9fc
jussimar <jussimar@jussimarleal.com.br>
2019-08-08 10:28:12 -0300

    alterando ola mundo

--- src/prjteste/PrjTeste.java
+++ src/prjteste/PrjTeste.java
@@ -15,7 +15,8 @@ public class PrjTeste {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Olá mundo! ");
+        System.out.println("Olá mundo - 2MIN Peruibe! ");
+        System.out.println("Teste");
     }
     
 }
